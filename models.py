2bf17508a2cac0b1e55830337b9962e5d10f7ad2
SirSanewa <lukaszsanewski@gmail.com>
2020-08-01 09:38:48 +0200

    DB init

---
+++ models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = "person"
+
+    id = Column(Integer, autoincrement=True, unique=True)
+    title = Column(String(100))
+    firstname = Column(String(100))
+    lastname = Column(String(100))
+    gender = Column(String(100))
+    street_nr = Column(String(100))
+    street_name = Column(String(100))
+    city = Column(String(100))
+    state = Column(String(100))
+    country = Column(String(100))
+    postcode = Column(String(100))
+    location_lat = Column(String(100))
+    location_lon = Column(String(100))
+    time_offser = Column(String(100))
+    time_description = Column(String(100))
+    email = Column(String(100))
+    uuid = Column(String(100))
+    username = Column(String(100))
+    password = Column(String(100))
+    salf = Column(String(100))
+    md5 = Column(String(100))
+    sha1 = Column(String(100))
+    sha256 = Column(String(100))
+    dob = Column(DateTime)
+    age = Column(Integer)
+    registered_date = Column(DateTime)
+    registered_age = Column(Integer)
+    phone = Column(String(100))
+    cell = Column(String(100))
+    id_name = Column(String(100))
+    id_value = Column(String(100))
+    nationality = Column(String(100))
+
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///database.db')
+    Base.metadata.create_all(engine)
