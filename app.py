b111cf96775b1dd6cf555fb38fb7532982f71fd5
SirSanewa <lukaszsanewski@gmail.com>
2020-08-05 09:35:33 +0200

    avarage age function

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from sqlalchemy import func
+
 from models import Person
 from session import session_creator
 import argparse
@@ -24,12 +26,35 @@ def gender_proportions():
     print(f"Gender proportions are: \n\t-{men_percent}% men({men} total)\n\t-{women_percent}% women({women} total).")
 
 
+def average_age(option="total"):
+    populations = {"total", "male", "female"}
+    if option not in populations:
+        raise ValueError("Used unsupported population")
+
+    if option == "total":
+        age_avg = sql_session.query(func.avg(Person.age)).scalar()
+    else:
+        age_avg = sql_session.query(func.avg(Person.age)).filter(Person.gender == option).scalar()
+
+    if age_avg.is_integer():
+        age_avg = int(age_avg)
+    else:
+        age_avg = round(age_avg, 2)
+    print(f"Average age for the selected population({option}) is {age_avg} years.")
+
+
 if __name__ == "__main__":
     sql_session = session_creator()
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("-g", "--gender_proportions", action="store_true",
+    parser.add_argument("-g", "--gender_proportion", action="store_true",
                         help="Returns gender proportions of the population")
+    parser.add_argument("-a", "--average_age", choices=["total", "male", "female"], const="total", nargs="?",
+                        help="Returns average age of the selected population from [male, female, total]. Default total")
 
     args = parser.parse_args()
-    if args.gender_proportions:
+
+    if args.sex_proportion:
         gender_proportions()
+    if args.average_age:
+        average_age(args.average_age)
